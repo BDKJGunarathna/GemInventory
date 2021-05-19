537f7bc1ec7b286a9c68275f0a7c6cd1746c15b8
LENOVO <jamisulochana@gmail.com>
2021-05-19 15:14:04 +0530

    Add alert message to update controller.

--- src/jewelryinventoryfunction/UpdatejewelrydetailsController.java
+++ src/jewelryinventoryfunction/UpdatejewelrydetailsController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -197,6 +198,11 @@ public class UpdatejewelrydetailsController implements Initializable {
 
         if(event.getSource() == donebtn3) {
             updateRecord();
+            Alert alert=new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Update Success!");
+            alert.setHeaderText(null);
+            alert.setContentText("Jewelry Price updated successfully!");
+            alert.showAndWait();
         }
 
     }
